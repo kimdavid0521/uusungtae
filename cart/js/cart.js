30d75459cf9c2a876f095c75daee13b00dc69aba
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2025-03-18 15:36:37 +0900

    ✨ feat: 제목 클릭스 이벤트 추가

--- cart/js/cart.js
+++ cart/js/cart.js
@@ -3,3 +3,10 @@ const buyButton = document.getElementById("buy_button");
 buyButton.addEventListener("click", () => {
   console.log("sdsd");
 });
+
+const title = document.getElementById("title");
+
+title.addEventListener("click", () => {
+  title.style.backgroundColor = "blue";
+  title.style.color = "white";
+});
